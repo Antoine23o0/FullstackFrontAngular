8d99876c01f21eb4119f96a911e45a732df61914
said <saidkamalshinwari098@gmail.com>
2024-03-28 10:27:22 +0100

    ajouter les joueurs depuis un fichier

--- src/app/joueurs/joueurs_ajout.component.ts
+++ src/app/joueurs/joueurs_ajout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { JoueursService } from "../joueurs.service";
 import { NgFor } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import * as Papa from 'papaparse';
 
 @Component({
   selector: 'app-joueur-ajout',
@@ -15,7 +16,6 @@ export class JoueurAjoutComponent implements OnInit {
   constructor(private joueurService: JoueursService) {
 
   }
-
   ngOnInit(): void {
     this.loadJoueur();
   }
@@ -43,5 +43,42 @@ export class JoueurAjoutComponent implements OnInit {
       console.warn(reponse);
       this.loadJoueur(); // Reload joueurs after adding
     });
+  }
+
+  importerJoueurs(event: any) {
+    const files = event.target.files;
+    if (files.length === 0) return;
+
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      // Utilisation de Papa Parse pour parser le fichier CSV
+      Papa.parse(fileReader.result as string, {
+        header: true,
+        skipEmptyLines: true,
+        complete: (result: { data: any[]; }) => {
+          console.log(result);
+          result.data.forEach(joueur => {
+            // Structurez votre joueur ici selon le format attendu par votre backend
+            const joueurData = {
+              categorie: [
+                { age: joueur.age },
+                { niveau: joueur.niveau }
+              ],
+              nom: joueur.nom,
+              point: joueur.point,
+              prenom: joueur.prenom,
+              sexe: joueur.sexe
+            };
+
+            // Envoi du joueur au backend
+            this.joueurService.ajouterJoueur(joueurData).subscribe((reponse) => {
+              console.warn(reponse);
+              this.loadJoueur(); // Reload joueurs after importing
+            });
+          });
+        }
+      });
+    };
+    fileReader.readAsText(files[0]);
   }
 }
